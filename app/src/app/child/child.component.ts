74dc9f4d88af54ef29a41f81ba9fca1888c8b9af
unknown <alevi0106@gmail.com>
2021-11-08 20:02:34 +0530

    LifeCycle Hooks

--- app/src/app/child/child.component.ts
+++ app/src/app/child/child.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy, OnChanges {
+
+  counter = 0;
+  interval : any;
+  @Input()
+  channel = "";
 
   constructor() { 
     console.log("Child constructor");
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Child Onchanges is called");
+    console.log(changes);
+  }
+  ngOnDestroy(): void {
+    console.log("Child Destroy");
+    clearInterval(this.interval);
+  }
 
   ngOnInit(): void {
     console.log("Child Init");
+    this.interval = setInterval(()=>{
+      this.counter += 1;
+      // console.log(this.counter);
+    }, 1000);
   }
 
 }
--- app/src/app/parent/parent.component.ts
+++ app/src/app/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  isChild = true;
+  channelName = 'Vaibhav';
   constructor() { 
     console.log("Parent constructor");
   }
 
   ngOnInit(): void {
     console.log("Parent Init");
+  }
+
+  displayChild(){
+    this.isChild = !this.isChild;
   }
 }
